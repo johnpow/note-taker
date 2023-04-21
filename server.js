afc480f287dda13e152bc8cb49a5fd3a3b1d1aaa
John Pow <johnowed@gmail.com>
2023-04-20 18:51:56 -0700

    fixed database reading and writing

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 let db = require('./db/db')
 const id = require('./helpers/id');
 const PORT = process.env.PORT || 3001;
+const fs = require('fs');
 
 const app = express();
 
@@ -24,20 +25,75 @@ app.get('/notes', function(req, res) {
   });
 
 app.get('/api/notes', (req,res) => {
-    res.json(db);
+  fs.readFile('./db/db.json', 'utf8', (err, notes) => {
+    if (err) {
+      return res.status(500).json({err});
+    }
+
+    res.json(JSON.parse(notes));
+  });
+
 })
 
 app.post('/api/notes', (req,res) => {
+
+  fs.readFile('./db/db.json', 'utf8', (err, notes) => {
+    //  check for errors if any happened
+    if (err) {
+      return res.status(500).json({err});
+    }
+
+    const data = JSON.parse(notes);
+    //  add data to the array from users.json file
+
     req.body.id = id();
-    db.push(req.body)
-    res.json(db);
+    data.push(req.body)
+    //  write the new array to the users.json file
+    fs.writeFile('./db/db.json', JSON.stringify(data, null, 2), (err) => {
+
+      if (err) {
+        return res.status(500).json({err});
+      }
+        //  send newly added data to the front-end
+      res.json(JSON.parse(notes));
+    });
+
+
+  });
+
 })
 
 app.delete('/api/notes/:id', (req,res) => {
-     db = db.filter(object => {
-        return object.id !== req.params.id;
+
+  fs.readFile('./db/db.json', 'utf8', (err, notes) => {
+    //  check for errors if any happened
+    if (err) {
+      return res.status(500).json({err});
+    }
+
+    let data = JSON.parse(notes);
+    //  add data to the array from users.json file
+
+
+     data = data.filter(object => {
+      return object.id !== req.params.id;
     });
-    res.json(db);
+
+    //  write the new array to the users.json file
+    fs.writeFile('./db/db.json', JSON.stringify(data, null, 2), (err) => {
+
+      if (err) {
+        return res.status(500).json({err});
+      }
+        //  send newly added data to the front-end
+      res.json(JSON.parse(notes));
+    });
+
+
+  });
+
+
+
 })
 
 
